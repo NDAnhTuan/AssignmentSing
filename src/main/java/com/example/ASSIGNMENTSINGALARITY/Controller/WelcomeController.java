c3e4ea499ddca146e872b448369113b5612d35f8
Your Name <nguyenducanhtuan0602@gmail.com>
2024-05-29 16:03:04 +0700

    change sql

--- src/main/java/com/example/ASSIGNMENTSINGALARITY/Controller/WelcomeController.java
+++ src/main/java/com/example/ASSIGNMENTSINGALARITY/Controller/WelcomeController.java
@@ -1,9 +1,11 @@
 package com.example.ASSIGNMENTSINGALARITY.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v1")
 public class WelcomeController {
     @GetMapping("/welcome")
     public String welcome() {
